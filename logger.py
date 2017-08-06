521e018f44ada5ad4f1fe7f786cd9f6cec195cfc
fynn <fynnay@gmail.com>
2017-08-06 22:40:25 +0200

    equal indents, setVerbosity, simpler handler+formatter setup

--- logger.py
+++ logger.py
@@ -6,123 +6,105 @@ from datetime import datetime
 import logging
 import colorlog
 
-class _Logger(object):
+from random import choice
+
+class ContextFilter(logging.Filter):
+    """
+    This is a filter which injects contextual information into the log:
+    - Add whitespace after loglevels to get more readable logs
+    - Custom 'parentFunction'
+    """
+    def equalIndent(self,record):
+        if record.levelname == "DEBUG":
+            outp = "DEBUG   "
+        elif record.levelname == "INFO":
+            outp = "INFO    "
+        elif record.levelname == "WARNING":
+            outp = "WARNING "
+        elif record.levelname == "ERROR":
+            outp = "ERROR   "
+        elif record.levelname == "CRITICAL":
+            outp = "CRITICAL"
+        return outp
+
+    def filter(self, record):
+        record.topFunc = getParentFunc(top=True)
+        # How many levels to step to reach the function that called one of the log functions (debug, info etc.)
+        record.parentFunc = getParentFunc(ancestor=6)
+        # Equal indent beginning
+        record.lvl = self.equalIndent(record)
+        return True
+
+class _Logger():
     '''
         NAME
             _Logger - For simple logging and generating a logfile
 
         DESCRIPTION
-            # Create a new logger:
-            LOG = _Logger("logger-name")
-            
-            # Log stuff at different levels:
-            LOG.debug("something")
-            LOG.info("something")
-            LOG.warning("something")
-            LOG.error("something")
-            LOG.critical("something")
-            LOG.exception("something")
-
-            # Change level of the console logger
-            By default only errors, critical and exceptions are written to the console.
-            If you want to show more levels call any of the following
-                'debug' | 'info' | 'warning' | 'error' | 'critical'
-            with LOG.setLevel('debug'):
-
-            # Disable/Enable the file handler
-            By default the logs are only shown in the console.
-            To write a full-level log you need to:
-            - Define an absolute filepath:
-                LOG.filePath = "/path/to/log/logFileName.log"
-            - Enable the logger
-                LOG.enableFileHandler(True)
-
-            To disable the fileHandler (this won't delete any previously created logfiles):
-            LOG.enableFileHandler(False)
-
-            The filename will always start with the calling.
-            The header 'File' will always be the topmost script.
-            That means, if a different filename is specified, the 'File' in the header
-            will still be the highest ancestor to make it easier to trace errors back to the file that caused them.
+            For all who want clean, readable logs but don't want to spend time setting them up.
 
         DATA
             logger
-                Access the logger to change advanced settings.
+                See :py:func:'logging.getLogger' or :py:class:'logging.Logger'
 
             filePath
-                Path to the current log-file. You need to set this before enabling the file handler.
-            
-            formatter
-                Access the Formatter to change format of the logs using logging.Formatter syntax.
-                Example:
-                    self.formatter = logging.Formatter("%(asctime)s:%(msecs)d - %(funcName)s - %(lineno)d - %(levelname)s : %(message)s","%H:%M:%S")
+                Path of the log-file.
+
+            color
+                Enable/Disable using color in console log.
+
+            verbosityLevel
+                see docstring of function 'setVerbosity'
 
         FUNCTIONS:
             enableFileHandler
 
     '''
-    def __init__(self,name,filePath=None,color=True):
+    def __init__(self,name,filePath=None,color=True,verbosity=0):
+        # Set color usage
+        self.useColor = color
         # Name the logger
         if color is True:
             self.logger = colorlog.getLogger(name)
         else:
             self.logger = logging.getLogger(name)
 
+        # Add filters
+        f = ContextFilter()
+        self.logger.addFilter(f)
+
         # Prevent loggers from sending their output to loggers created earlier, thus displaying the same output twice.
         self.logger.propagate = False
         
         # Set Log level
         self.logger.setLevel(logging.DEBUG)
-
-        # Create substitute functions for log methods so they can be accessec at the base of the class
-        # self.debug     = self.logger.debug
-        # self.info      = self.logger.info
-        # self.warning   = self.logger.warning
-        # self.error     = self.logger.error
-        # self.critical  = self.logger.critical
-        # self.exception = self.logger.exception
-
-        # Create header to be written at the top of the .log file
-        topScript = getParentCaller(top=True)[0]
-        self.header    = "---- LOG ----\nFile  : %s\nDate  : %s\nHost  : %s\nOS    : %s\n\n%s\n\n"%\
-        (topScript,
-        datetime.now(),
-        platform.uname()[1],
-        platform.uname()[0].lower(),
-        "<level> : <script name> :: <time> - <function name> - <line> >> <custom message>")
         
-        # Create Color Formatter
-        self.colorFormatter = colorlog.ColoredFormatter('%(log_color)s%(levelname)s : %(name)s :: %(funcName)s >> %(message)s')
-        # Set colors
-        self.colorFormatter.log_colors['DEBUG'] = 'green'
-        self.colorFormatter.log_colors['INFO'] = 'white'
-        # Create Regular Formatter
-        self.chFormatter    = logging.Formatter('%(levelname)s : %(name)s :: %(funcName)s : %(message)s')
-        self.formatter      = logging.Formatter('%(levelname)s : %(name)s :: %(asctime)s.%(msecs)d - %(funcName)s - %(lineno)d >> %(message)s',"%H:%M:%S")
+        # Set formatter according to console verbosity
+        self.chVerbosity = verbosity
+        self.chFormatter    = None
+        self.fhFormatter    = None
 
-        # Filepath used by the FileHandler. Has to be set before enabling the FileHandler
-        self.filePath = filePath
-        self.fh       = None
-
-        # Create console handler
-        self.chVerbose = False # Switch console handler log level between DEBUG and ERROR
+        # Console handler
         self.ch = colorlog.StreamHandler()
-        if color is True:
-            self.ch.setFormatter(self.colorFormatter)
-        else:
-            self.ch.setFormatter(self.chFormatter)
+        self.setVerbosity()
         self.enableConsoleHandler(True)
 
+        # File handler
+        self.fh = None
+        self.filePath = filePath
+        fhStr = ["%(levelname)s : %(name)s :: %(asctime)s.%(msecs)d - %(parentFunc)s - %(lineno)d >> %(message)s","%H:%M:%S"]
+        self.fhFormatter = logging.Formatter(fhStr[0],fhStr[1])
+
     def formatForLogging(self,inp):
-            msg = " ".join( [str(i) for i in inp] )
-            return msg
+        msg = " ".join( [str(i) for i in inp] )
+        return msg
 
     def debug(self,*args,**kwargs):
         return self.logger.debug(self.formatForLogging(args))
 
     def info(self,*args,**kwargs):
         return self.logger.info(self.formatForLogging(args))
-    
+
     def warning(self,*args,**kwargs):
         return self.logger.warning(self.formatForLogging(args))
 
@@ -133,7 +115,8 @@ class _Logger(object):
         return self.logger.critical(self.formatForLogging(args))
 
     def exception(self,*args,**kwargs):
-        return self.logger.exception(self.formatForLogging(args))
+        # Use 'error' level to avoid parentFunc detection problems
+        return self.logger.error(self.formatForLogging(args),exc_info=True)
 
     def enableConsoleHandler(self,state):
         # Check if there is already a StreamHandler
@@ -148,10 +131,21 @@ class _Logger(object):
             self.ch.setLevel(100)
         elif state is True:
             self.ch.setLevel(logging.ERROR)
-            if self.chVerbose is True:
-                self.ch.setLevel(logging.DEBUG)
+
+    def getHeader(self):
+        topScript = getParentScript(top=True)[0]
+        header    = "---- LOG ----\nFile  : %s\nDate  : %s\nHost  : %s\nOS    : %s\n\n"%\
+        (topScript,
+        datetime.now(),
+        platform.uname()[1],
+        platform.uname()[0].lower())
+        return header
 
     def enableFileHandler(self,state):
+        '''
+        Enable/disable fileHandler.
+        You have to setFilePath before you can do this.
+        '''
         if state is False:
             # Remove any file handlers
             for h in self.logger.handlers:
@@ -159,14 +153,14 @@ class _Logger(object):
                     self.logger.removeHandler(h)
             self.fh = None
         elif state is True:
+            # Check if filePath is set
             if self.filePath is None:
                 raise ValueError("self.filePath is None. You need to set it before enabling the fileHandler.")
                 return
-            # Write header to file, if it doesn't exist yet.
-            if not os.path.exists(self.filePath):
-                tempfile = open(self.filePath,'a')
-                tempfile.write(self.header)
-                tempfile.close()
+            # Append header to file
+            tempfile = open(self.filePath,'a')
+            tempfile.write(self.getHeader())
+            tempfile.close()
             # Check if there is already a FileHandler
             fhExists = False
             for h in self.logger.handlers:
@@ -175,22 +169,16 @@ class _Logger(object):
             if fhExists == False:
                 self.fh = logging.FileHandler(self.filePath)
                 self.fh.setLevel(logging.DEBUG)
-                self.fh.setFormatter(self.formatter)
+                self.fh.setFormatter(self.fhFormatter)
                 self.logger.addHandler(self.fh)
         else:
             raise ValueError("Invalid State. Can only be True or False")
 
-    def verbose(self,state):
+    def setFilePath(self,inp):
         '''
-        Change Verbosity of the console handler.
+        Sets filePath variable.
         '''
-        if state is False:
-            self.ch.setLevel(logging.ERROR)
-        elif state is True:
-            self.ch.setLevel(logging.DEBUG)
-            self.chVerbose = True
-        else:
-            raise ValueError("Invalid State. Can only be True or False")
+        self.filePath = inp
 
     def setLevel(self,inpState):
         '''
@@ -209,10 +197,97 @@ class _Logger(object):
             self.ch.setLevel(logging.CRITICAL)
         elif isinstance(state,int):
             self.ch.setLevel(state)
+        else:
+            raise ValueError("No such state '%s'"%inpState)
 
-def getParentCaller(top=False):
+    def setVerbosity(self,level=0):
+        '''
+        Set amount of information displayed by the console handler:
+        0  : level + message
+        10 : level + functionName + message
+        20 : level + fileName + functionName + message
+        30 : level + fileName + functionName + line + message
+        '''
+        if not isinstance(level,int):
+            raise ValueError("level must be <int>")
+
+        self.chVerbosity = level
+        
+        # Console Formatter
+        chStr = ""
+        if self.useColor is True:
+            chStr += "%(log_color)s"
+        
+        # Build message
+        if self.chVerbosity >= 0 :
+            chStr += "%(lvl)s"
+        if self.chVerbosity >= 20 :
+            chStr += " : "
+            chStr += "%(name)s"
+        if self.chVerbosity >= 10 :
+            chStr += " : "
+            chStr += "%(parentFunc)s"
+        if self.chVerbosity >= 30 :
+            chStr += " : "
+            chStr += "%(lineno)d"
+        chStr += " >> "
+        chStr += "%(message)s"
+        
+        if self.useColor:
+            self.chFormatter = colorlog.ColoredFormatter(chStr)
+            # Set colors
+            self.chFormatter.log_colors['DEBUG'] = 'green'
+            self.chFormatter.log_colors['INFO'] = 'white'
+        else:
+            self.chFormatter = logging.Formatter(chStr)
+        self.ch.setFormatter(self.chFormatter)
+
+
+def getParentFunc(top=False,ancestor=0):
+    """
+    Returns the name of the function that called this function.
+    param: ancestor : <int> : Positive values get older ancestors, negative values get younger ancestors. Stops when reaching <module> level.
+    param: top : <bool>     : Get the oldest ancestor that is a function. Stops before reaching <module> level.
+
+    Excample:
+    def first():
+        return second()
+
+    def second():
+        print( getParentFunc() )
+        print( getParentFunc(ancestor=1) )
+        print( getParentFunc(ancestor=2) )
+        print( getParentFunc(ancestor=-1) )
+        print( getParentFunc(top=True) )
+        return
+    first()
+    # Output:
+    >>> second
+    >>> first
+    >>> <module>
+    >>> getParentFunc
+    >>> first
+    """
+    insp = inspect.getouterframes( inspect.currentframe() )
+    if top is True:
+        ret = insp[len(insp)-2][3]
+    else:
+        pos = 1+ancestor
+        # Make sure pos is not more or lens than len(insp)
+        while True:
+            if pos >=len(insp):
+                pos -= 1
+            elif pos < 0:
+                pos+=1
+            else:
+                break
+        ret = insp[pos][3]
+    
+    return ret
+
+def getParentScript(top=False):
     '''
-    Gets the path to the script
+    Gets the path to the script that called this function
     '''    
     # Inspect current call stack
     insp    = inspect.getouterframes(inspect.currentframe(),2)
@@ -228,14 +303,15 @@ def getParentCaller(top=False):
 def test():
     LOG = _Logger("logging_howto.py")
     LOG.setLevel('debug')
+    LOG.setVerbosity(10)
 
     # Create some logs from lowest to highest level
     LOG.debug("I have",99, "problems","but a",type(()),"ain't one.")
-    LOG.info('info','15')
+    LOG.info('info')
     LOG.warning('warning')
     LOG.error('error')
     LOG.critical('critical')
-    LOG.exception("exception")
+    LOG.exception("lol")
 
 # HOW TO USE
 if __name__ == "__main__":
