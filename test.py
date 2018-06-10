3f68ed765049e4b58f96b7b88932504e5d918475
Fynn Laue <lachhaft@gmail.com>
2018-06-10 12:10:59 +0200

    moved test file to root

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 # file: test.py
 # info: Demonstration of how to use neatlog
 #==============================
-from . import neatlog
+import neatlog
 
 def test():
     # Create some logs from lowest to highest level
@@ -11,12 +11,13 @@ def test():
     LOG.warning('warning')
     LOG.error('error')
     LOG.critical('critical')
-    try: 1/0
+    try:
+    	1/0
     except:
         LOG.exception("lol")
 
 if __name__ == '__main__':
-    LOG = neatlog._Logger("logging_howto",color=True)
+    LOG = neatlog.getLogger("logging_howto",level='error', color=True)
     LOG.setLevel('debug')
     LOG.setVerbosity(30)
     LOG.debug('test')
