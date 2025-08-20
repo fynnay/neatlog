6641c39719de50fcf5490cbe585510ebd7ea6743
fynn <fynn-git.wi04u@slmail.me>
2025-08-20 15:52:55 +0200

    test getting existing logger

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,45 @@
 import pytest
 
+import neatlog
+
 
 @pytest.fixture
-def logger_name() -> str:
+def f_logger_name() -> str:
     return "MY_LOGGER"
+
+
+@pytest.fixture
+def f_logger(
+        f_logger_name,
+) -> neatlog.neatlog._Logger:
+    return neatlog.neatlog._Logger(f_logger_name)
+
+
+@pytest.fixture
+def level_notset_str():
+    return "notset"
 
 
 @pytest.fixture
 def level_debug_str():
     return "debug"
+
+
+@pytest.fixture
+def level_info_str():
+    return "info"
+
+
+@pytest.fixture
+def level_warning_str():
+    return "warning"
+
+
+@pytest.fixture
+def level_error_str():
+    return "error"
+
+
+@pytest.fixture
+def level_exception_str():
+    return "exception"
